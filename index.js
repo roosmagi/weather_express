d19a31e6a89080770e11e7a97af1c0d8a1a92ea4
Angela Roosmägi <angela.roosmagi@voco.ee>
2024-10-07 10:56:41 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const fetch = require('node-fetch')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// proceed form post method data
+app.use(express.json())
+app.use(express.urlencoded({extended : true} ))
+
 const key = '698535dd1ac0f5b08aaaef7b91f309b2';
 let city = 'Tartu'
 
@@ -25,4 +29,23 @@ app.get("/", function(req, res) {
         })
     })
 })  
+
+app.post('/', function(req, res) {
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((responce) => {
+        return responce.json()
+    }) 
+    .then((data) =>{
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render('index',{
+            description: description,
+            city: city,
+            temp: temp
+        }) 
+    })
+})
+
 app.listen(3004)
